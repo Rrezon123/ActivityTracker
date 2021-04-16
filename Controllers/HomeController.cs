63bb169cc66a4378eb8d787852287bd91eb7516c
Rrezon <rrezonpllana@gmail.com>
2021-04-16 14:45:24 +0200

    Starting to make it responsive commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,47 +19,70 @@ namespace ActivityTracker.Controllers
         }
         private readonly ILogger<HomeController> _logger;
 
-       private ToDoContext _context;
+        private ToDoContext _context;
 
-        public HomeController(ILogger<HomeController> logger,ToDoContext context)
+        public HomeController(ILogger<HomeController> logger, ToDoContext context)
         {
             _logger = logger;
-           _context=context;
+            _context = context;
         }
         [Route("Dashboard")]
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            //  if (loggedInUser == null)
-            //     return RedirectToAction("Index", "SignIn");
-
+             if (loggedInUser == null)
+                return RedirectToAction("Index", "SignIn");
+            ViewBag.User=_context.Users.FirstOrDefault(u=>u.UserId==loggedInUser.UserId);
             return View();
         }
         [HttpGet("ToDo")]
         public IActionResult ToDo()
         {
-            //  if (loggedInUser == null)
-            //     return RedirectToAction("Index", "SignIn");
+            if (loggedInUser == null)
+                return RedirectToAction("Index", "SignIn");
+
+            ViewBag.ShowToDo = _context.ToDos.FirstOrDefault(todo => todo.ToDoId == HttpContext.Session.GetInt32("todoId"));
             return View();
         }
         [HttpGet("Teams")]
         public IActionResult Teams()
         {
-            //  if (loggedInUser == null)
-            //     return RedirectToAction("Index", "SignIn");
+             if (loggedInUser == null)
+                return RedirectToAction("Index", "SignIn");
+
             return View();
         }
 
 
         [HttpPost("AddToDo")]
-        public IActionResult AddToDo(ToDo newTodo){
-            if(ModelState.IsValid){
+        public IActionResult AddToDo(ToDo newTodo)
+        {
+            if (ModelState.IsValid)
+            {
                 _context.ToDos.Add(newTodo);
                 _context.SaveChanges();
-                return RedirectToAction("ToDo");   
+                HttpContext.Session.SetInt32("todoId", newTodo.ToDoId);
+                return RedirectToAction("ToDo");
             }
             return View("AddPartial");
 
+        }
+        [HttpPost("Update/{todoid}")]
+        public IActionResult UpdateToDo(int todoid)
+        {
+            ToDo editedToDo = _context.ToDos.FirstOrDefault(todo => todo.ToDoId == todoid);
+            if (editedToDo.Status == 1)
+            {
+                editedToDo.Status = 2;
+            }
+            else
+            {
+                editedToDo.Status=3;
+            }
+
+            editedToDo.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+            return RedirectToAction("ToDo");
         }
         public IActionResult Privacy()
         {
